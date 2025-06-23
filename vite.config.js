eaeddea07497125317550e1b987e292c7ba52be7
Fredson Junior <fredsonjunior5209@gmail.com>
2025-06-23 13:49:00 -0500

    Atualiza configuração do Vite para incluir a base do repositório

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ADICIONE ESTA LINHA:
-  base: '/meu-portfolio-repo/', 
+  base: "/meu-portfolio-repo/", // <--- ADICIONE ESTA LINHA
   plugins: [react()],
 })
